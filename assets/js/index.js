74f08ccda6f3e1412355cf88ffeb2abcb7a12507
Rory Simmonds <rory@blankbox.business>
2024-01-25 18:58:35 +0000

    add starter js files

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+$(function() {
+  const API = fdeabcbe2a5f2a291f6081b9c648f575;
+  let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon={lon}&appid=${API}`
+  // https://openweathermap.org/forecast5
+})
